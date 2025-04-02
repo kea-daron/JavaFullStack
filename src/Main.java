de34b5b8c9e9dfde9c46e1f3c467a14889a1e0bd
kea-daron <daron3327@gmail.com>
2025-04-02 10:22:22 +0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.printf("%f", Math.PI);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("[+] insert name : ");
+        String name = sc.nextLine();
+        System.out.print("[+] insert age : ");
+        int age = sc.nextInt();
+        System.out.print("[+] insert gender : ");
+        char gender = sc.next().charAt(0);
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Gender : " + gender);
     }
 }
