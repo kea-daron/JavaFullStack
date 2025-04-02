7d820f9c0d523b04ce1dc4bdb64a38c9279b4145
kea-daron <daron3327@gmail.com>
2025-04-02 12:00:08 +0700

    operator lession

---
+++ src/operator/Main.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 11;
+        b+=a;// b = b+a; => b = 11+12
+        int c = 12;
+        int d = 11;
+        b-=a;// b = b - a => b = 11-12;
+
+    }
+}
