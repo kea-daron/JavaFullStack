86847ce87d1972138b5c63273881bf56583d9efd
kea-daron <daron3327@gmail.com>
2025-04-03 11:52:07 +0700

    ControlFlow Statemnet

---
+++ src/controlFlowStatement/swtich/Main.java
@@ -0,0 +1,13 @@
+package controlFlowStatement.swtich;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username : ");
+        String username = scanner.nextLine();
+        System.out.println("Enter your password : ");
+        String password = scanner.nextLine();
+    }
+}
