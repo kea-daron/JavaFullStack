ed8d5c8f42827d8826677c308ed8ed2cb23bb676
kea-daron <daron3327@gmail.com>
2025-04-04 12:06:18 +0700

    Switch case

--- src/controlFlowStatement/swtich/Main.java
+++ src/controlFlowStatement/swtich/Main.java
@@ -4,10 +4,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your username : ");
-        String username = scanner.nextLine();
-        System.out.println("Enter your password : ");
-        String password = scanner.nextLine();
+        System.out.print("បញ្ចូលលេខទី១ : ");
+        int លេខទី១ = new Scanner(System.in).nextInt();
+        System.out.print("បញ្ចូលលេខទី២ : ");
+        int លេខទី២ = new Scanner(System.in).nextInt();
+        System.out.println("=" .repeat(20));
+        System.out.println("""
+                1. បូកលេខ
+                2. ដកលេខ
+                3. រកលេខដែលធំជាង
+                4. ផ្លាសប្តូរលេខ
+                5. ចាកចេញ
+                """);
+        System.out.println("=" .repeat(20));
+        System.out.print("បញ្ចូលជម្រើស : ");
+        int ជម្រើស = new Scanner(System.in).nextInt();
+        switch (ជម្រើស){
+            case 1->{
+                int បូក = លេខទី១ + លេខទី២;
+                System.out.println("តម្លៃសរុប : "+បូក);
+                break;
+            }
+            case 2->{
+                int ដក = លេខទី១ - លេខទី២;
+                System.out.println("តម្លៃសរុប : "+ដក);
+                break;
+            }
+            case 3->{
+                if (លេខទី១ > លេខទី២){
+                    System.out.println("លេខទី១ធំជាងលេខទិ២");
+                }else {
+                    System.out.println("លេខទី២ធំជាងលេខទិ១");
+                }
+            }
+            case 4->{
+                int លេខជំនួស;
+                លេខជំនួស = លេខទី១;
+                លេខទី១ = លេខទី២;
+                លេខទី២ = លេខជំនួស;
+                System.out.println("លេខទី១ = " + លេខទី១);
+                System.out.println("លេខទី១ = " + លេខទី២);
+            }
+            case 5->{
+                return;
+            }
+        }
     }
 }
